1979c92b3510e7e20ecd6020e78209ac44746e4b
Anwarus <Anwarus1289@gmail.com>
2019-03-08 21:00:29 +0100

    (feature) - main commands structures

---
+++ bin/index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const dispatcher = require('./../lib/dispatcher');
+
+const actions = {
+    add: 'add',
+    edit: 'edit',
+    delete: 'delete',
+    show: 'show',
+    list: 'list'
+};
+
+program
+    .command(`${actions.add} <object>`)
+    .description('Add new object to list')
+    .action(function action(object) {
+        dispatcher.parse(object, actions.add);
+    });
+
+program
+    .command(`${actions.edit} <object>`)
+    .description('Edit new object to list')
+    .action(function action(object) {
+        dispatcher.parse(object, actions.edit);
+    });
+
+program
+    .command(`${actions.delete} <object>`)
+    .description('Delete new object to list')
+    .action(function action(object) {
+        dispatcher.parse(object, actions.delete);
+    });
+
+program
+    .command(`${actions.show} <object>`)
+    .description('Show new object to list')
+    .action(function action(object) {
+        dispatcher.parse(object, actions.show);
+    });
+
+program
+    .command(`${actions.list} <object>`)
+    .description('List new object to list')
+    .action(function action(object) {
+        dispatcher.parse(object, actions.list);
+    });
